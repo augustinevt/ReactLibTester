39ea561a3f4f508629ebc3a002a125bd2c1ccbc3
august von trapp <augustvontrapp@gmail.com>
2019-09-29 18:35:27 -0700

    branch off

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import {PatternManager} from 'sequencer'
+
+
 function App() {
+
+console.log("->", PatternManager)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PatternManager
+        handler={(cells) => console.log(cells)}
+      />
+      <PatternManager
+        handler={(cells) => console.log(cells)}
+      />
+      <PatternManager
+        handler={(cells) => console.log(cells)}
+      />
     </div>
   );
 }
